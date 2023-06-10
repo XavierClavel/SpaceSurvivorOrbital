e48069fa0c5aeccc2bb945e2c7da4800249f7803
Xavier Clavel <xrclavel@gmail.com>
2023-06-10 18:08:43 +0200

    rollback to base font

--- Assets/Scripts/Ennemies/Ennemy.cs
+++ Assets/Scripts/Ennemies/Ennemy.cs
@@ -81,10 +81,6 @@ public class Ennemy : MonoBehaviour
         ObjectManager.dictObjectToEnnemy.Add(gameObject, this);
     }
 
-    private void Update()
-    {
-        if (knockback) Debug.Log(rb.velocity);
-    }
 
     protected virtual void FixedUpdate()
     {
@@ -104,6 +100,7 @@ public class Ennemy : MonoBehaviour
 
     public void Hurt(int damage, status effect, bool critical)
     {
+        Debug.Log(damage);
         Sequence sequence = DOTween.Sequence();
         sequence.Append(spriteOverlay.DOColor(Color.white, 0.1f));
         sequence.Append(spriteOverlay.DOColor(Helpers.color_whiteTransparent, 0.1f));
