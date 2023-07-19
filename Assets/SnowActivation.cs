f5af1a3c8c86759627801536611b3731e9fe7e7e
Florian <massat.florian@gmail.com>
2023-07-19 15:15:39 +0200

    Snow effect on Ice World

---
+++ Assets/SnowActivation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowActivation : MonoBehaviour
+{
+    public GameObject player; // Référence vers le transform du joueur
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+
+        if (player != null)     
+        {
+            // Suivre la position du joueur
+            transform.position = new Vector2(player.transform.position.x, player.transform.position.y +10);
+        }
+    }
+}
