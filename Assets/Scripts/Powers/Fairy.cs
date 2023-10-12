2b9c64bcfa40404bb0e4210563be08f496a2f64c
Florian <massat.florian@gmail.com>
2023-10-12 10:15:40 +0200

    Fairy plug Ok

--- Assets/Scripts/Powers/Fairy.cs
+++ Assets/Scripts/Powers/Fairy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fairy : MonoBehaviour
+public class Fairy : Power
 {
     // Start is called before the first frame update
     void Start()
