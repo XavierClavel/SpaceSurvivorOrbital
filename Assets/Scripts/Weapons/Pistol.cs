f42c58deef08aae38dac8747ebfac946fe3ce6f3
Xavier Clavel <xrclavel@gmail.com>
2023-11-19 16:29:09 +0100

    push : upgrade to fire bullets in different directions

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * <pre>
+ * <p>IntA -> Mode (0 : normal, 1 : shoots on two sides, 2 : shoots on 4 sides)</p>
+ * </pre>
+ */
 public class Pistol : Gun
 {
 
@@ -17,7 +22,26 @@ public class Pistol : Gun
     {
         if (stats.projectiles == 1)
         {
-            FireBullet(firePoint.position, firePoint.eulerAngles);
+            Vector3 position = firePoint.position;
+            Vector3 eulerRotation = firePoint.eulerAngles;
+            switch (fullStats.generic.intA)
+            {
+                case 0 : 
+                    FireBullet(position, eulerRotation);
+                    return;
+            
+                case 1 :
+                    FireBullet(position, eulerRotation);
+                    FireBullet(position, eulerRotation + 180f * Vector3.forward);
+                    return;
+                case 2 :
+                    FireBullet(position, eulerRotation);
+                    FireBullet(position, eulerRotation + 90f * Vector3.forward);
+                    FireBullet(position, eulerRotation + 180f * Vector3.forward);
+                    FireBullet(position, eulerRotation + 270f * Vector3.forward);
+                    return;
+            
+            }
             return;
         }
 
@@ -46,6 +70,7 @@ public class Pistol : Gun
     {
         Vector3 position = firePoint.position + i * distanceOffsetBetweenBullets * firePoint.right;
         Vector3 eulerRotation = firePoint.eulerAngles + i * stats.spread * Vector3.forward;
+        
         FireBullet(position, eulerRotation);
     }
 
