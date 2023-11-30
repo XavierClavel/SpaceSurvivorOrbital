2fa469da179897b912e78c6b91c183b0c3158bfb
Florian <massat.florian@gmail.com>
2023-11-30 15:10:15 +0100

    Onde sonore

--- Assets/SynthWave.cs
+++ Assets/SynthWave.cs
@@ -7,19 +7,22 @@ public class SynthWave : Power
     [SerializeField] private Shockwave shockwave;
 
     private bool isShockwaveEnabled;
+    private bool doubleShockwave;
     private int shockwaveDamage;
     private float shockwaveMaxRange;
     private status shockwaveElement;
 
     public override void Setup(PlayerData _)
     {
-        base.Setup(_)
+        base.Setup(_);
         isShockwaveEnabled = _.generic.boolA;
         shockwaveMaxRange = _.generic.floatA;
         shockwaveDamage = _.generic.intA;
         shockwaveElement = _.generic.elementA;
+        doubleShockwave = _.generic.boolB;
 
-        InvokeRepeating("DoShockwave", 0f, 2f);
+        InvokeRepeating("DoShockwave", 0f, stats.attackSpeed);
+        if (doubleShockwave) { InvokeRepeating("DoShockwave", 0.5f, stats.attackSpeed); }
     }
 
     protected override void Start()
