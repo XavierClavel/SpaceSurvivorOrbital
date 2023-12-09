0d84e78127db0484806e72365838ca31a7795dd2
Florian <massat.florian@gmail.com>
2023-12-09 15:33:03 +0100

    Change Export

--- Assets/Scripts/BlackHole.cs
+++ Assets/Scripts/BlackHole.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackHole : Power
+public class BlackHole : PowerHandler
 {
     // Start is called before the first frame update
     void Start()
