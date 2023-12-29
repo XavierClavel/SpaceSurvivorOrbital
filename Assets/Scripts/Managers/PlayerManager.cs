cb025b1edfe6a1e9308ad8d48fe5ecaa257e10e3
Florian <massat.florian@gmail.com>
2023-12-29 16:35:53 +0100

    Test

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -77,6 +77,7 @@ public class PlayerManager
 
     public static void setCharacter(CharacterHandler characterHandler)
     {
+        //characterPrefab = characterHandler.getCharacter();
         character = characterHandler;
     }
 
