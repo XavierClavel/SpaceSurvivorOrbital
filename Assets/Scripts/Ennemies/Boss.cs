44d9f3a8b90d7671627b83a71a73defba146e668
Florian <massat.florian@gmail.com>
2024-02-06 17:25:16 +0100

    Boss_Visual

--- Assets/Scripts/Ennemies/Boss.cs
+++ Assets/Scripts/Ennemies/Boss.cs
@@ -6,15 +6,20 @@ public class Boss : Ennemy
 {
     private ComponentPool<Bullet> poolBullets;
     [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] ParticleSystem waveExplosionPS;
     private const float bulletSpeed = 6f;
     private const float bulletLifetime = 5f;
     private const int amountMultiBullets = 16;
     private float waveSpread;
     private const float bulletSpread = 10f;
+    private SpriteRenderer spriteRenderer;
+    private float timeBeforeWave = 10f;
+    private bool waveDone;
 
     protected override void Start()
     {
         base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         maxHealth = 1000 * PlanetSelector.getDifficulty();
         health = maxHealth;
         healthBar = ObjectManager.getBossHealthbar();
@@ -25,17 +30,27 @@ public class Boss : Ennemy
         waveSpread = 360f / amountMultiBullets;
         poolBullets = new ComponentPool<Bullet>(bulletPrefab);
         InvokeRepeating(nameof(FireTowardsPlayer),2f,1f);
-        InvokeRepeating(nameof(FireBulletsWave), 5f, 10f);
+        InvokeRepeating(nameof(FireBulletsWave), timeBeforeWave, 10f);
+        StartCoroutine(nameof(waitPS));
+    }
+
+    IEnumerator waitPS()
+    {
+        yield return new WaitForSeconds(8);
+        waveExplosionPS.Play();
     }
 
     private void FireTowardsPlayer()
     {
+        
         FireBullets(transform.getRotationTo(player.transform).z, health > maxHealth * 0.5f ? 1 : 3,  bulletSpread);
     }
 
     private void FireBulletsWave()
     {
         FireBullets(0f, amountMultiBullets,waveSpread);
+        waveExplosionPS.Stop();
+        StartCoroutine(nameof(waitPS));
     }
 
     private void FireBullets(float rotation, int amountBullets, float spread)
@@ -62,7 +77,7 @@ public class Boss : Ennemy
     
     private void FireBullet(float rotation)
     {
-        poolBullets.get(transform.position + Vector3.back,  rotation * Vector3.forward)
+        poolBullets.get(transform.position + Vector3.back + new Vector3 (0, -2, 0),  rotation * Vector3.forward)
             .setPool(poolBullets)
             .Fire(bulletSpeed, bulletLifetime, baseDamage.getRandom());
     }
