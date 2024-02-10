230f83958ad853d409c9c4220823581bdb1f068b
Xavier Clavel <xrclavel@gmail.com>
2024-02-10 09:34:55 +0100

    fixed charge over transition

--- Assets/Scripts/UI/SceneTransitionManager.cs
+++ Assets/Scripts/UI/SceneTransitionManager.cs
@@ -43,12 +43,12 @@ public class SceneTransitionManager : MonoBehaviour
     
     public void LocalTransitionToScene(gameScene newScene)
     {
-        
         SoundManager.onSceneChange(newScene);
         overlay.SetActive(true);
         imageTransform.sizeDelta = new Vector2(canvasX, canvasY);
         maskTransform.sizeDelta = Vector2.zero;
-        maskTransform.DOSizeDelta(2 * canvasX * Vector2.one, 1f)
+        maskTransform
+            .DOSizeDelta(2 * canvasX * Vector2.one, 1f)
             .SetEase(Ease.InOutQuint)
             .SetUpdate(true)
             .SetDelay(0.2f)
