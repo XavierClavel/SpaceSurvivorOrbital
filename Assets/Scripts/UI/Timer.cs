8dc23a8a2e803b0a82ea54104c2f7181a82cf317
Florian <massat.florian@gmail.com>
2024-05-23 17:24:10 +0200

    timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,71 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
-using DG.Tweening;
-using Shapes;
 
-public class Timer : MonoBehaviour
+public class TimerSlider : MonoBehaviour
 {
-    [SerializeField] Ennemy boss;
-    
-    public static int timeRemainingToAdd;
-    private float timeRemaining;
+    public Slider timerSlider; // Associez ceci avec le Slider dans l'inspecteur
+    public float timerDuration = 10f; // Durée du timer en secondes
 
-    private float elapsedTime = 0.0f; // Temps écoulé en secondes
-
-
-    [SerializeField] private Image timerDisplay;
-    [SerializeField] private Transform skullPivotTransform;
-    [SerializeField] private Transform skullTransform;
-    
-    
-    public GameObject winText;
-    private float factor;
-
-    private float startAngle = 0f;
-    private float endAngle = -65f;
-
-
-    
+    private float timer;
+    private bool isTimerRunning = false;
 
     void Start()
     {
-        factor = 1f / timeRemaining;
-        if (!DebugManager.doNoTimer()) StartCoroutine(nameof(TimerRunner));
-        float angle = startAngle + (endAngle - startAngle) * factor;
-    }
-    
-    private IEnumerator TimerRunner()
-    {
-        while (true)
+        if (timerSlider != null)
         {
-            timeRemaining = ConstantsData.timerDuration;
-            while (timeRemaining > 0)
-            {
-                timeRemaining -= Time.fixedDeltaTime;
-                //timerSlider.value = timeRemaining * factor;
-                //disc.AngRadiansEnd = endAngle + (startAngle - endAngle) * timeRemaining * factor;
-                timerDisplay.fillAmount = 1 - timeRemaining * factor;
-                //skullPivotTransform.eulerAngles = endAngle * (1 - timeRemaining * factor) * Vector3.forward;
-                //skullTransform.eulerAngles = Vector3.zero;
-                yield return Helpers.getWait(Time.fixedDeltaTime);
-            }
-            PlanetSelector.IncreaseDifficulty(true);
+            timerSlider.maxValue = timerDuration;
+            ResetTimer();
+            isTimerRunning = true;
         }
-        //Vector3 bossPosition = PlayerController.instance.transform.position + Helpers.getRandomPositionInRadius(new Vector2(4f, 8f));
-        //Instantiate(boss, bossPosition, Quaternion.identity).onDeath.AddListener(OnBossDefeat);
+        else
+        {
+            Debug.LogError("Slider non assigné dans l'inspecteur.");
+        }
     }
 
-
-    private void OnBossDefeat()
+    void Update()
     {
-        PauseMenu.instance.PauseGame(false);
-        Instantiate(winText);
-        //InputManager.setSelectedObject(firstSelected);
+        if (isTimerRunning)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                timerSlider.value = timer;
+            }
+            else
+            {
+                isTimerRunning = false;
+                Invoke(nameof(ResetTimer), 0); // Réinitialiser le timer après affichage du message
+            }
+        }
     }
 
+    private void ResetTimer()
+    {
+        timer = timerDuration;
+        timerSlider.value = timerDuration;
+        isTimerRunning = true;
+    }
 }
