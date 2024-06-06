501e60ffdba2a392154bd2bb10db10ceb56367b3
Florian <massat.florian@gmail.com>
2024-06-06 17:42:16 +0200

    Tile Obstacle

---
+++ Assets/OrderLayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderLayer : MonoBehaviour
+{
+    private float order;
+    public SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        order = -transform.position.y + 2000 + transform.position.x;
+        sprite.sortingOrder = (int) order;
+    }
+}
