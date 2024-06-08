4982022e3c80bb2f66f86fa4d47ed1748ad3a262
Florian <massat.florian@gmail.com>
2024-06-08 12:05:20 +0200

    Tuile obstacle Ice and Forest

--- Assets/OrderLayer.cs
+++ Assets/OrderLayer.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class OrderLayer : MonoBehaviour
 {
     private float order;
-    public SpriteRenderer sprite;
+    private SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
