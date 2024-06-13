8e046348c4920ac6527d82e984abde563b1fb896
Florian <massat.florian@gmail.com>
2024-06-13 20:02:05 +0200

    FlameThrower

--- Assets/Scripts/Powers/PowerFlame.cs
+++ Assets/Scripts/Powers/PowerFlame.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PowerFlame : Power
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = player.transform.position;
+        // Obtenir la direction de visée depuis le PlayerController
+        Vector2 aimDirection = player.aimVector;
+
+        if (aimDirection != Vector2.zero)
+        {
+            // Calculer l'angle en radians
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+
+            // Appliquer la rotation
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
     }
 }
