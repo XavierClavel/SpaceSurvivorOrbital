eeb309d9416922f0b00d07900cdd7ecc9eb192cb
Xavier Clavel <xrclavel@gmail.com>
2024-07-10 20:36:01 +0200

    hold to buy on data selector

--- Assets/Scripts/UI/Title Screen/SelectButton.cs
+++ Assets/Scripts/UI/Title Screen/SelectButton.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,7 +6,7 @@ using Shapes;
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class SelectButton : MonoBehaviour, ISelectHandler
+public class SelectButton : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     private Button button;
     [HideInInspector] public RectTransform rectTransform;
@@ -23,18 +24,30 @@ public class SelectButton : MonoBehaviour, ISelectHandler
     [SerializeField] private Color colorFilled;
     [SerializeField] private Color colorUnfilled;
     [SerializeField] ParticleSystem unlockedPS;
+    private bool isSelected = false;
+    private InputMaster inputActions;
 
 
     private void Awake()
     {
         button = GetComponent<Button>();
         rectTransform = GetComponent<RectTransform>();
+        
+        inputActions = new InputMaster();
+        inputActions.UI.Validate.started += ctx => startHolding();
+        inputActions.UI.Validate.canceled += ctx => stopHolding();
+    }
+
+    private void OnDestroy()
+    {
+        inputActions.Disable();
     }
 
     public void Setup(string key, SelectorLayout selectorLayout)
     {
         this.key = key;
         this.selectorLayout = selectorLayout;
+        gameObject.name = key;
 
         isUnlocked = SaveManager.isOptionUnlocked(key);
 
@@ -59,6 +72,24 @@ public class SelectButton : MonoBehaviour, ISelectHandler
 
         DataSelector.instance.dictKeyToButton[key] = this;
     }
+    
+    
+    public void startHolding()
+    {
+        if (isUnlocked) return;
+        StartCoroutine(nameof(holdCoroutine));
+    }
+
+    public void stopHolding()
+    {
+        StopCoroutine(nameof(holdCoroutine));
+    }
+    
+    private IEnumerator holdCoroutine()
+    {
+        yield return Helpers.getWait(1.5f);
+        Buy();
+    }
 
 
     public void Select()
@@ -80,11 +111,13 @@ public class SelectButton : MonoBehaviour, ISelectHandler
         if (rectangleFill == null) return;
         rectangleFill.Color = colorUnfilled;
         discFill.Color = colorUnfilled;
+        stopHolding();
     }
 
 
     public void Buy()
     {
+        Debug.Log("paying");
         if (!DataSelector.Transaction(cost)) return;
         
         SaveManager.unlockOption(key);
@@ -107,6 +140,13 @@ public class SelectButton : MonoBehaviour, ISelectHandler
 
     public void OnSelect(BaseEventData eventData)
     {
+        Debug.Log($"OnSelect - {gameObject.name}");
+        inputActions.Enable();
         DataSelector.DisplayGeneric(key, this);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        inputActions.Disable();
     }
 }
